8dcb6ed071bb1bf18e431f6d231d6a96ecd27db1
ariel murphy <arielladders@gmail.com>
2020-07-28 23:48:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,8 +31,17 @@ class Song
   end
 
   def self.artist_count 
-    
+    @@artists.collect do |artist|
+      if @@artist_count.include?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
+    end
+    @@artist_count
   end
+  
+  
 
   def initialize(name, artist, genre)
     @name = name 
